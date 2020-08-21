c687566a1a1b9ec294fd7e7468ee417b7f92566d
Robin payot <robin@upperquad.com>
2020-08-21 16:46:17 +0200

    start fix local

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,13 @@
 const w = require('webpack-merge')
 const common = require('./webpack.common.js')
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
 module.exports = w.merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    // contentBase: buildDir,
+    contentBase: path.resolve(__dirname, 'build'),
     compress: true,
     port: PORT,
     watchOptions: {
